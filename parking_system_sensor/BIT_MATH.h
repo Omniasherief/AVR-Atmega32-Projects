570bf30bc46679bf4880116965defd10e702697c
Omniasherief <102552702+Omniasherief@users.noreply.github.com>
2024-07-18 20:19:52 +0300

    Delete parking_system_sensor directory

--- parking_system_sensor/BIT_MATH.h
+++
@@ -1,6 +0,0 @@
-
-#define SET_BIT(REG,BIT_NUM) REG |=( 1<< BIT_NUM)
-#define CLR_BIT(REG,BIT_NUM) REG &=(~( 1<< BIT_NUM))
-
-#define TOG_BIT(REG,BIT_NUM) REG ^=( 1<< BIT_NUM)
-#define GET_BIT(REG,BIT_NUM) ((REG >> BIT_NUM)&1)
