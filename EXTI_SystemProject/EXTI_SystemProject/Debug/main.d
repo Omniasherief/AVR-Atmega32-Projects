d1e0d5e5025c184ce4bb49fc331da36bcd5f1d1c
Omniasherief <102552702+Omniasherief@users.noreply.github.com>
2024-07-18 20:56:06 +0300

    Add files via upload

---
+++ EXTI_SystemProject/EXTI_SystemProject/Debug/main.d
@@ -0,0 +1,21 @@
+main.o main.o: ../main.c ../STD_TYPES.h ../ErrType.h ../PORT_interface.h \
+  ../DIO_interface.h ../CLCD_interface.h ../KPD_interface.h \
+  ../EXTI_interface.h ../GIE_interface.h ../DCMotor_interface.h
+
+../STD_TYPES.h:
+
+../ErrType.h:
+
+../PORT_interface.h:
+
+../DIO_interface.h:
+
+../CLCD_interface.h:
+
+../KPD_interface.h:
+
+../EXTI_interface.h:
+
+../GIE_interface.h:
+
+../DCMotor_interface.h:
